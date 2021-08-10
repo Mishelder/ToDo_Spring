6a078ead87249cc45a570d3eccfc2d5f1ba2f657
Mishelder <mi3h1@mail.ru>
2021-08-10 20:01:01 +0300

    Add mainfilter to filter requests. Closes #3.

---
+++ src/main/java/com/miaskor/todo_spring/filter/MainFilter.java
@@ -0,0 +1,57 @@
+package com.miaskor.todo_spring.filter;
+
+import com.miaskor.todo_spring.model.Client;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+
+import static com.miaskor.todo_spring.util.URLs.*;
+
+@Component
+public class MainFilter extends HttpFilter {
+
+    private static final Set<String> PUBLIC_PAGES = getAllPublicURL();
+    private static final Set<String> ALL_FEASIBLE_PAGES = getAllFeasibleURL();
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        req.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        res.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        var client = (Client) req.getSession().getAttribute("client");
+        var cookies = req.getCookies();
+        var uri = req.getRequestURI();
+
+        if (PUBLIC_PAGES.stream().anyMatch(uri::startsWith)) {
+            chain.doFilter(req, res);
+        }else if(cookies!=null && loggedIn(client, findCookieLoggedIn(cookies))
+                &&ALL_FEASIBLE_PAGES.stream().anyMatch(uri::startsWith)){
+            chain.doFilter(req, res);
+        } else {
+            res.sendRedirect(req.getHeader("referer") == null
+                    ? LOGIN :req.getHeader("referer"));
+        }
+    }
+
+    private boolean findCookieLoggedIn(Cookie[] cookies) {
+        Optional<Cookie> loggedInCookie = Arrays.stream(cookies)
+                .filter((cookie -> cookie.getName().equals("loggedIn")))
+                .findFirst();
+        return loggedInCookie.map(cookie -> cookie.getValue().equals("true")).orElse(false);
+    }
+
+    private boolean loggedIn(Client client, boolean cookieLoggedIn) {
+        return Objects.nonNull(client)
+                && cookieLoggedIn;
+    }
+}
---
+++ src/main/java/com/miaskor/todo_spring/util/URLs.java
@@ -0,0 +1,18 @@
+package com.miaskor.todo_spring.util;
+
+import lombok.experimental.UtilityClass;
+
+import java.util.Set;
+
+@UtilityClass
+public class URLs {
+    public static final String LOGIN = "/login";
+
+    public static Set<String> getAllFeasibleURL() {
+        return Set.of(LOGIN);
+    }
+
+    public static Set<String> getAllPublicURL() {
+        return Set.of(LOGIN);
+    }
+}
