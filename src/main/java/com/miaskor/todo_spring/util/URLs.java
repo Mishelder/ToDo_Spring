67b20021a140bd57fa782eba29640b1b479500c7
Mishelder <mi3h1@mail.ru>
2021-08-11 23:49:06 +0300

    Add url to public pages. Closes #5.

--- src/main/java/com/miaskor/todo_spring/util/URLs.java
+++ src/main/java/com/miaskor/todo_spring/util/URLs.java
@@ -7,12 +7,13 @@ import java.util.Set;
 @UtilityClass
 public class URLs {
     public static final String LOGIN = "/login";
+    public static final String CSS_FILES = "/css";
 
     public static Set<String> getAllFeasibleURL() {
         return Set.of(LOGIN);
     }
 
     public static Set<String> getAllPublicURL() {
-        return Set.of(LOGIN);
+        return Set.of(LOGIN,CSS_FILES);
     }
 }
