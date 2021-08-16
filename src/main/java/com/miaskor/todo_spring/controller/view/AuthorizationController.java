e450e62250b9bf0817c33e84040dfc8da920c5bf
Mishelder <mi3h1@mail.ru>
2021-08-16 23:18:53 +0300

    adding registration clients. Closes #9.

--- src/main/java/com/miaskor/todo_spring/controller/view/AuthorizationController.java
+++ src/main/java/com/miaskor/todo_spring/controller/view/AuthorizationController.java
@@ -1,5 +1,8 @@
 package com.miaskor.todo_spring.controller.view;
 
+import com.miaskor.todo_spring.exception.ClientEmailHasAlreadyExistException;
+import com.miaskor.todo_spring.exception.ClientIsNotExistException;
+import com.miaskor.todo_spring.exception.ClientLoginHasAlreadyExistException;
 import com.miaskor.todo_spring.model.Client;
 import com.miaskor.todo_spring.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,21 +13,55 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class AuthorizationController {
 
     private final ClientService clientService;
+    private final HttpSession httpSession;
 
     @Autowired
-    public AuthorizationController(ClientService clientService){
+    public AuthorizationController(ClientService clientService, HttpSession httpSession) {
         this.clientService = clientService;
+        this.httpSession = httpSession;
     }
 
     @PostMapping("/login")
     @Transactional(readOnly = true)
     public @ResponseBody Client loginClient(@RequestBody Client client){
-        clientService.findByLogin(client.getLogin());
-        return clientService.findByLoginAndPassword(client.getLogin(), client.getPassword());
+        var byLogin = clientService.findByLogin(client.getLogin());
+        if(byLogin.isEmpty())
+            throw new ClientIsNotExistException("Client with login = "
+                    + client.getLogin() + " hasn't found");
+        var byLoginAndPassword = clientService
+                .findByLoginAndPassword(client.getLogin(), client.getPassword());
+        if(byLoginAndPassword.isEmpty())
+            throw new ClientIsNotExistException("Client with password = "
+                    + client.getPassword() + " hasn't found");
+        httpSession.setAttribute("client",client);
+        return byLoginAndPassword.get();
+    }
+
+    @PostMapping("/registration")
+    @Transactional()
+    public @ResponseBody Client registrationClient(@RequestBody Client client){
+        var byLogin = clientService.findByLogin(client.getLogin());
+        if(byLogin.isPresent())
+            throw new ClientLoginHasAlreadyExistException("Client with login = "
+                    + client.getLogin() + " has already exist");
+        var byEmail = clientService.findByEmail(client.getEmail());
+        if(byEmail.isPresent())
+            throw new ClientEmailHasAlreadyExistException("Client with email = "
+                    + client.getEmail() + " has already exist");
+        httpSession.setAttribute("client",client);
+        return clientService.save(client);
+    }
+
+    @GetMapping("/logout")
+    public String loggedOut(){
+        httpSession.invalidate();
+        return "authorization";
     }
 
     @GetMapping("/login")
